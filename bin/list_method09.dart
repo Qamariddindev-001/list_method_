91a78e96dc1a146a892137431573dff5d71b7f57
User <qamariddinol664@gmail.com>
2023-01-23 14:35:30 +0500

    bin/list_metod09.dart code

--- bin/list_method09.dart
+++ bin/list_method09.dart
@@ -6,5 +6,19 @@
     Returns:
         list: return answer
 */
+List fruits(List list) {
+  List x = [];
+  int y = 0;
+  for (int i = 0; i < list.length; i++) {
+    if (list[i] == 'apple') {
+      x.add(i);
+      y++;
+    }
+  }
+  x.insert(0, y);
+  return x;
+}
 
-void main() {}
+void main() {
+  print(fruits(["apple", "banana", "apple", "pear", "apple"]));
+}
